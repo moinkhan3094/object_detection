8cd9375e9e1b4b8b9eeef0bbb3b081ab33919550
moin khan <59043660+moinkhan3094@users.noreply.github.com>
2021-01-21 14:22:07 +0530

    Add files via upload

---
+++ dog vs cat (1).py
@@ -0,0 +1,595 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+from tqdm import tqdm
+
+
+# In[3]:
+
+
+DATADIR="C:/Users/moink/Desktop/mnist/PetImages"
+
+
+# In[4]:
+
+
+CATEGORIES=["Dog","Cat"]
+
+
+# In[5]:
+
+
+for category in CATEGORIES:  
+    path = os.path.join(DATADIR,category) 
+    for img in os.listdir(path): 
+        img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  
+        plt.imshow(img_array, cmap='gray') 
+        plt.show()  
+        break  
+    break  
+
+
+# In[6]:
+
+
+print(img_array)
+
+
+# In[7]:
+
+
+print(img_array.shape)
+
+
+# In[8]:
+
+
+IMG_SIZE = 50
+
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap='gray')
+plt.show()
+
+
+# In[9]:
+
+
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap='gray')
+plt.show()
+
+
+# In[10]:
+
+
+training_data = []
+
+def create_training_data():
+    for category in CATEGORIES:  
+
+        path = os.path.join(DATADIR,category) 
+        class_num = CATEGORIES.index(category)  
+
+        for img in tqdm(os.listdir(path)): 
+            try:
+                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  
+                training_data.append([new_array, class_num])  
+            except Exception as e:  
+                pass
+            
+create_training_data()
+
+print(len(training_data))
+
+
+# In[12]:
+
+
+import random
+random.shuffle(training_data)
+
+
+# In[13]:
+
+
+for sample in training_data[:10]:
+    print(sample[1])
+
+
+# In[14]:
+
+
+X = []
+y = []
+
+for features,label in training_data:
+    X.append(features)
+    y.append(label)
+
+print(X[0].reshape(-1, IMG_SIZE, IMG_SIZE, 1))
+
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+
+# In[18]:
+
+
+import pickle
+
+pickle_out = open("X.pickle","wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle","wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+
+# In[19]:
+
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+
+# In[51]:
+
+
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+
+import time
+import pickle
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+
+model = Sequential()
+
+model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(256, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())  
+
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+time.sleep(0.1)
+model.fit(X, y, batch_size=32, epochs=3, validation_split=0.3)
+
+
+# In[45]:
+
+
+from tensorflow.keras.callbacks import TensorBoard
+
+
+# In[46]:
+
+
+NAME = "Cats-vs-dogs-CNN"
+
+
+# In[47]:
+
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+
+# In[48]:
+
+
+model.fit(X, y,
+          batch_size=32,
+          epochs=3,
+          validation_split=0.3,
+          callbacks=[tensorboard])
+
+
+# In[29]:
+
+
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+
+from tensorflow.keras.callbacks import TensorBoard
+import pickle
+import time
+
+NAME = "Cats-vs-dogs-CNN"
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+
+model = Sequential()
+
+model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(256, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())  
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'],
+              )
+
+model.fit(X, y,
+          batch_size=32,
+          epochs=3,
+          validation_split=0.3,
+          callbacks=[tensorboard])
+
+
+# In[30]:
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+
+from tensorflow.keras.callbacks import TensorBoard
+import pickle
+import time
+
+NAME = "Cats-vs-dogs-64x2-CNN"
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+
+model = Sequential()
+
+model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten()) 
+model.add(Dense(64))
+model.add(Activation('relu'))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'],
+              )
+
+model.fit(X, y,
+          batch_size=32,
+          epochs=10,
+          validation_split=0.3,
+          callbacks=[tensorboard])
+
+
+# In[31]:
+
+
+import time
+
+dense_layers = [0,1,2]
+layer_sizes = [32, 64, 128]
+conv_layers = [1, 2, 3]
+
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
+            print(NAME)
+
+
+# In[32]:
+
+
+dense_layers = [0, 1, 2]
+layer_sizes = [32, 64, 128]
+conv_layers = [1, 2, 3]
+
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
+            print(NAME)
+
+            model = Sequential()
+
+            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            for l in range(conv_layer-1):
+                model.add(Conv2D(layer_size, (3, 3)))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            model.add(Flatten())
+            for _ in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation('relu'))
+
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
+
+
+# In[33]:
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+
+from tensorflow.keras.callbacks import TensorBoard
+import pickle
+import time
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+
+dense_layers = [0, 1, 2]
+layer_sizes = [32, 64, 128]
+conv_layers = [1, 2, 3]
+
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
+            print(NAME)
+
+            model = Sequential()
+
+            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            for l in range(conv_layer-1):
+                model.add(Conv2D(layer_size, (3, 3)))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            model.add(Flatten())
+
+            for _ in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation('relu'))
+
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
+
+            tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+            model.compile(loss='binary_crossentropy',
+                          optimizer='adam',
+                          metrics=['accuracy'],
+                          )
+
+            model.fit(X, y,
+                      batch_size=32,
+                      epochs=10,
+                      validation_split=0.3,
+                      callbacks=[tensorboard])
+
+
+# In[34]:
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+
+from tensorflow.keras.callbacks import TensorBoard
+import pickle
+import time
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+
+dense_layers = [0]
+layer_sizes = [64]
+conv_layers = [3]
+
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
+            print(NAME)
+
+            model = Sequential()
+
+            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            for l in range(conv_layer-1):
+                model.add(Conv2D(layer_size, (3, 3)))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            model.add(Flatten())
+
+            for _ in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation('relu'))
+
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
+
+            tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+            model.compile(loss='binary_crossentropy',
+                          optimizer='adam',
+                          metrics=['accuracy'],
+                          )
+
+            model.fit(X, y,
+                      batch_size=32,
+                      epochs=10,
+                      validation_split=0.3,
+                      callbacks=[tensorboard])
+
+model.save('64x3-CNN.model')
+
+
+# In[35]:
+
+
+import cv2
+import tensorflow as tf
+
+CATEGORIES = ["Dog", "Cat"] 
+
+
+def prepare(filepath):
+    IMG_SIZE = 70  
+    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE) 
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)  
+
+
+# In[36]:
+
+
+model = tf.keras.models.load_model("64x3-CNN.model")
+
+
+# In[37]:
+
+
+prediction = model.predict([prepare('dog.jpg')])  
+
+
+# In[ ]:
+
+
+prediction
+
+
+# In[ ]:
+
+
+prediction[0][0]
+
+
+# In[ ]:
+
+
+print(CATEGORIES[int(prediction[0][0])])
+
+
+# In[ ]:
+
+
+prediction = model.predict([prepare('cat.jpg')])
+print(prediction) 
+print(CATEGORIES[int(prediction[0][0])])
+
+
+# In[38]:
+
+
+import cv2
+import tensorflow as tf
+
+CATEGORIES = ["Dog", "Cat"]
+
+
+def prepare(filepath):
+    IMG_SIZE = 70  
+    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+
+model = tf.keras.models.load_model("64x3-CNN.model")
+
+prediction = model.predict([prepare('doggo.jpg')])
+print(prediction)  
+print(CATEGORIES[int(prediction[0][0])])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
